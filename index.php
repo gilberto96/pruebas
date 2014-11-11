0e71e547c5a716df1a48ca488658d2f1a553d7c4
gilberto96 <gilbertorojas1213@gmail.com>
2014-11-11 00:30:51 -0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<?php 
+$indicesServer = array('PHP_SELF', 
+'argv', 
+'argc', 
+'GATEWAY_INTERFACE', 
+'SERVER_ADDR', 
+'SERVER_NAME', 
+'SERVER_SOFTWARE', 
+'SERVER_PROTOCOL', 
+'REQUEST_METHOD', 
+'REQUEST_TIME', 
+'REQUEST_TIME_FLOAT', 
+'QUERY_STRING', 
+'DOCUMENT_ROOT', 
+'HTTP_ACCEPT', 
+'HTTP_ACCEPT_CHARSET', 
+'HTTP_ACCEPT_ENCODING', 
+'HTTP_ACCEPT_LANGUAGE', 
+'HTTP_CONNECTION', 
+'HTTP_HOST', 
+'HTTP_REFERER', 
+'HTTP_USER_AGENT', 
+'HTTPS', 
+'REMOTE_ADDR', 
+'REMOTE_HOST', 
+'REMOTE_PORT', 
+'REMOTE_USER', 
+'REDIRECT_REMOTE_USER', 
+'SCRIPT_FILENAME', 
+'SERVER_ADMIN', 
+'SERVER_PORT', 
+'SERVER_SIGNATURE', 
+'PATH_TRANSLATED', 
+'SCRIPT_NAME', 
+'REQUEST_URI', 
+'PHP_AUTH_DIGEST', 
+'PHP_AUTH_USER', 
+'PHP_AUTH_PW', 
+'AUTH_TYPE', 
+'PATH_INFO', 
+'ORIG_PATH_INFO') ; 
+
+echo '<table cellpadding="10">' ; 
+foreach ($indicesServer as $arg) { 
+    if (isset($_SERVER[$arg])) { 
+        echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ; 
+    } 
+    else { 
+        echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ; 
+    } 
+} 
+echo '</table>' ;?>
+</body>
+</html>
